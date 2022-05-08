8850a5b8d8711cb29aa104812af00d58fd7c303e
pedro-negrao <pedro.negrao12@gmail.com>
2022-05-08 11:45:18 -0300

    Finalização do backend (APIs)

--- src/main/java/com/dio/santander/bankline/api/model/Movimentacao.java
+++ src/main/java/com/dio/santander/bankline/api/model/Movimentacao.java
@@ -2,6 +2,7 @@ package com.dio.santander.bankline.api.model;
 
 import java.time.LocalDateTime;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.EnumType;
 import javax.persistence.Enumerated;
@@ -20,10 +21,13 @@ public class Movimentacao {
     private LocalDateTime dataHora;
     private String descricao;
     private Double valor;
-    
+
     @Enumerated(EnumType.STRING)
     private MovimentacaoTipo tipo;
     
+    @Column(name = "id_conta")
+    private Integer idConta;
+
     public Integer getId() {
         return id;
     }
@@ -54,6 +58,11 @@ public class Movimentacao {
     public void setTipo(MovimentacaoTipo tipo) {
         this.tipo = tipo;
     }
-    
+    public Integer getIdConta() {
+        return idConta;
+    }
+    public void setIdConta(Integer idConta) {
+        this.idConta = idConta;
+    }
 
 }
