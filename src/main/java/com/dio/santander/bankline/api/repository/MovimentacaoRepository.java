399ad6cf49ee21a3e67cb460c7b79b75ada9de99
pedro-negrao <pedro.negrao12@gmail.com>
2022-05-11 20:05:20 -0300

    Adição de novos serviços para o frontend

--- src/main/java/com/dio/santander/bankline/api/repository/MovimentacaoRepository.java
+++ src/main/java/com/dio/santander/bankline/api/repository/MovimentacaoRepository.java
@@ -1,9 +1,12 @@
 package com.dio.santander.bankline.api.repository;
 
+import java.util.List;
+
 import com.dio.santander.bankline.api.model.Movimentacao;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface MovimentacaoRepository extends JpaRepository<Movimentacao, Integer> {
     
+    public List<Movimentacao>findByIdConta(Integer idConta);
 }
